d8053c56df1f2291eb5dbba1827a3fa30ba05dac
rao-rabi <rabirao1039@gmail.com>
2023-08-27 16:09:54 +0500

    first commit

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
 const styles = {
     bestOfYoutubeIcon :'d-flex align-items-center gap-4 justify-content-start mb-2 px-md-1 w-100 py-1 sideBarElement',
     mainYoutubeIcon : 'd-flex align-items-center gap-md-4 justify-content-start px-md-1 px-1 py-1 sideBarElement',
-    VideoTags:"btn btn-outline-secondary me-md-2 bg-body-secondary rounded-pill ps-3 pe-3 text-black text-nowrap"
+    VideoTags:"btn btn-outline-secondary me-2 bg-body-secondary rounded-pill ps-3 pe-3 text-black text-nowrap"
 }
 export {styles}
